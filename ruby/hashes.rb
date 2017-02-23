538d1c61c348b1cb45232c31b2c08ed593f4342a
Nicholas <nitrevino@davidson.edu>
2017-02-22 20:16:07 -0500

    Turned program into hashes

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,32 +1,56 @@
 interior_designer = {
-
-
-
-
+"name" => "",
+"age_int" => "",
+"children" => "",
+"decour_theme" => ""
 
 }
 puts "welcome to the assistant designer program!"
 
 puts "what is your name"
 name = gets.chomp
-interior_designer << name
+interior_designer["name"] = name
 
-until
+
+
 puts "what is your age"
 age = gets.chomp
 age_int = age.to_i
-interior_designer << age_int
+interior_designer["age_int"] = age
 
 
-
-end
 
 puts "How many children do you have"
 children = gets.chomp
-children_int = children.to_i
-interior_designer << children
-
+interior_designer["children"] = children
 
 puts "what decour theme do you want"
 decour_theme = gets.chomp
-interior_designer << decour_theme
+interior_designer["decour_theme"] = decour_theme
+
+#here are your responses
+p interior_designer
+
+puts "do you want to change any of your responses"
+responses = gets.chomp
+if responses = "yes" then
+ puts "type which one?"
+  responses2 = gets.chomp
+ if responses2 = "name " then
+     puts "what is your name"
+      name = gets.chomp
+    interior_designer["name"] = name
+
+  elsif responses2= "age"
+      age = gets.chomp
+      age_int = age.to_i
+      interior_designer["age_int"] = age
+  elsif responses2 = "children"
+      children = gets.chomp
+      interior_designer["children"] = children
+  elsif responses2  = "decour_theme"
+      interior_designer["decour_theme"] = decour_theme
+else
+  puts "ok goodbye now"
+end
+end 
