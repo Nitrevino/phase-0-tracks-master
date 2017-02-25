a80b784cf53187400c5d7b39368de56fa44be8c5
Nicholas <nitrevino@davidson.edu>
2017-02-25 15:08:33 -0500

    added data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,36 @@
+
+news = {
+    guardian: {
+        motto: 'First Class',
+        website_info: {
+            articles_per_day: 6000,
+            hits_per_day: 70,000
+        },
+        journalists: [
+            "Spencer Ackerman",
+            "Peter Bradshaw"
+        ]
+    },
+    wapo: {
+        motto: 'Business Class',
+        website_info: {
+            articles_per_day: 1100
+            hits_per_day: 60,000
+        },
+        journalists: [
+            "Dana Milibank",
+            "Christopher Michael Cillizza"
+        ]
+    },
+    mother_jones: {
+        motto: 'Coach',
+        website_info: {
+            articles_per_day: 500,
+            hits_per_day: 23,0000
+        },
+        journalists: [
+          "Keven Drum"
+          "Clara Jeffery"
+        ]
+    }
+}
