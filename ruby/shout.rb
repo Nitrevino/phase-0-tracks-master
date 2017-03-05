dce9299ed355ced656c7dc3fc336782fa871112f
Nicholas <nitrevino@davidson.edu>
2017-03-05 18:09:33 -0500

    Added happy and angry

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,7 +1,13 @@
 require_relative 'shout'
 Shout.singleton_methods
 
-
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!" + " :)"
+  end
+
 end
