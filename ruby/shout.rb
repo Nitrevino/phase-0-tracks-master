4ce9f3380e2d26dbf90e3665dbca1ab34a328579
Nicholas <nitrevino@davidson.edu>
2017-03-05 18:54:24 -0500

    Producers reference

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,35 @@
-require_relative 'shout'
-Shout.singleton_methods
-
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+#  def self.yell_happily(words)
+#    words + "!!!" + " :)"
+#  end
+#
+#end
+#Shout.yell_angrily("I'm angry")
+#Shout.yell_happily("I'm happy")
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+    def self.yell_angrily(words)
+      words + "!!!" + " :("
+    end
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
-  end
-
+    def self.yell_happily(words)
+      words + "!!!" + " :)"
+    end
 end
+
+class Happy_person
+  include Flight
+end
+
+class Angry_person
+  include Flight
+end
+
+angry = Angry_person.new
+happy.yell_angrily("I ALL ALONE BEWEEP MY OUTCAST STATE")
+
+happy = Happy_person.new
+angry.yell_happily("But under the right circumstances, a producer could make more money with a flop than he could with a hit.")
