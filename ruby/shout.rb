05a030e9c01fb8d59b6f5d5ca22fe61b99414297
Nicholas <nitrevino@davidson.edu>
2017-03-05 19:15:25 -0500

    got rid of the self thing because that was the only way I could get it to work

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -11,25 +11,25 @@
 #Shout.yell_angrily("I'm angry")
 #Shout.yell_happily("I'm happy")
 module Shout
-    def self.yell_angrily(words)
+    def yell_angrily(words)
       words + "!!!" + " :("
     end
 
-    def self.yell_happily(words)
+    def yell_happily(words)
       words + "!!!" + " :)"
     end
 end
 
 class Happy_person
-  include Flight
+  include Shout
 end
 
 class Angry_person
-  include Flight
+  include Shout
 end
 
-angry = Angry_person.new
-happy.yell_angrily("I ALL ALONE BEWEEP MY OUTCAST STATE")
-
 happy = Happy_person.new
-angry.yell_happily("But under the right circumstances, a producer could make more money with a flop than he could with a hit.")
+happy.yell_happily("But under the right circumstances, a producer could make more money with a flop than he could with a hit.")
+
+angry = Angry_person.new
+angry.yell_angrily("I ALL ALONE BEWEEP MY OUTCAST STATE")
