5de9f84a9ee5be9fb26331d13b23e03b9101bfe7
Nicholas <nitrevino@davidson.edu>
2017-03-06 02:05:05 -0500

    cried and then fixed the slice problem

--- ruby/game.rb
+++ ruby/game.rb
@@ -9,6 +9,7 @@ class Word_game
 
   def initialize
       puts "welcome to the word game"
+
   end
 
   def define_variable(word)
@@ -19,32 +20,62 @@ class Word_game
     @number_of_guess = @word.length
     @wordlength = @word.length
     @tries = 0
+
   end
 
   def underscore
-        word_box = []
-  @current =  "-"*@wordlength
-  word_box  << @current
+
+
  end
+
  def guess_a_letter(letter)
-   @letter = letter
-#if @split_word.include? @letter = TRUE
-if  @split_word.include? @letter
-puts "you got a letter right"
-@tries +=1
- else
-"you got a letter wrong"
-
-
-
+  @letter = letter
+  if  @split_word.include? @letter
+    puts "you got a letter right"
+     @tries +=1
+     @right_letter = @split_word.select{|a| a == @letter }
+     p @right_letter
+     p @right_letter
+  else
+   puts  "you got a letter wrong"
+    @tries +=1
 end
+ # word_box = []
+  #   @current =  "_"*@wordlength
+   # @current_current = @current.chars
+    #word_box  << @current_current
+    #p word_box
+
+    #split_word[index]
+
+    #@split_word.map!{ |x| x == @letter ? word_box : x }.flatten!
+
+  #@split_word.map!{ |x| x == @letter ? word_box : x }.flatten!
+  #p word_box
+  #p @split_word
 end
+
+#slice out the right letter
+#insert into word word_box by number index
+
+#def check_guess(tries)
+ #   if @tries == @word.index
+  #    print "you lose!"
+   # else puts "keep going"
+  #  end
+  #end
+#end
+
+
 # user interface
-
 puts "what word do you want to choose?"
 game = Word_game.new
 game.define_variable(gets.chomp)
 game.underscore
  puts "what letter do you want?"
 game.guess_a_letter(gets.chomp)
+#game.check_guess()
+
+#game.underscore
+
 end
