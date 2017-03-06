92b41c3cf3a47c3b3d9229d4413e8626d21684ac
Nicholas <nitrevino@davidson.edu>
2017-03-05 23:05:20 -0500

    split fix

--- ruby/game.rb
+++ ruby/game.rb
@@ -13,12 +13,28 @@ class Word_game
 
   def define_variable(word)
     @word = word
+    split_word = word.split(//)
+    p split_word
     @is_over = false
     @number_of_guess = @word.length
+    @wordlength = @word.length
   end
+
+  def underscore
+   word_box = []
+  @current =  "-"*@wordlength
+  word_box  << @current
+ end
+ def guess_a_letter(letter)
+
+
+end
 end
 # user interface
 
 puts "what word do you want to choose?"
 game = Word_game.new
 game.define_variable(gets.chomp)
+game.underscore
+ puts "what letter do you want?"
+game.guess_a_letter(gets.chomp)
